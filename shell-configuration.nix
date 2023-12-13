88a68b873c7e74f06667750f127040c7e78c7443
SavvyHex <paisaketh@gmail.com>
2023-12-13 22:37:37 +0530

    nixfiles folder

--- shell-configuration.nix
+++
@@ -1,7 +0,0 @@
-{
-  # Bash Aliases
-  environment.interactiveShellInit = ''
-	alias nixbuild="sudo nixos-rebuild switch -I nixos-config=/home/savvyhex/Projects/mynixos/configuration.nix"
-	alias nixedit="code ~/Projects/mynixos/ && exit"
-  '';
-}
