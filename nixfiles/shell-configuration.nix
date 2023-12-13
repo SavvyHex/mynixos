86f45d62e159bd7c7114f1bfb172424edb40c82a
SavvyHex <paisaketh@gmail.com>
2023-12-13 22:46:34 +0530

    custom neofetch

--- nixfiles/shell-configuration.nix
+++ nixfiles/shell-configuration.nix
@@ -3,5 +3,6 @@
   environment.interactiveShellInit = ''
 	alias nixbuild="sudo nixos-rebuild switch -I nixos-config=/home/savvyhex/Projects/mynixos/nixfiles/configuration.nix"
 	alias nixedit="code ~/Projects/mynixos/ && exit"
+  alias neofetch="neofetch --config ~/Projects/mynixos/neofetch/config.conf"
   '';
 }
