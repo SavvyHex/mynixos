5c803a011016ba2ff923a163bb4a397ffa6132fc
SavvyHex <paisaketh@gmail.com>
2023-12-17 21:59:32 +0530

    added creation and usage of venv to aliases

--- nixfiles/shell-configuration.nix
+++ nixfiles/shell-configuration.nix
@@ -4,5 +4,7 @@
 	alias nixbuild="sudo nixos-rebuild switch -I nixos-config=/home/savvyhex/Projects/mynixos/nixfiles/configuration.nix"
 	alias nixedit="code ~/Projects/mynixos/ && exit"
   alias neofetch="neofetch --config ~/Projects/mynixos/neofetch/config.conf"
+  alias venvinit="python -m venv env"
+  alias usevenv="source ./env/bin/activate"
   '';
 }
